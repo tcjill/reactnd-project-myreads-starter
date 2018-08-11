1dfd1a0b0731f50749bfa641121413a76671f006
jill <jgundaker@gmail.com>
2018-08-10 18:34:11 -0700

    current shelf addtion

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class BooksApp extends React.Component {
         <Route path="/search" render={() =>(
           <SearchPage
             moveShelf={this.moveShelf}
-            books={this.props.books}
+            books={this.state.books}
             />
         )}   />
             
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -58,12 +58,19 @@ class SearchPage extends Component {
                     <ol className="books-grid">
                         {
                          this.state.searchedBooks.map(searchedBook => {
-                             
+                            let shelf = "none";
+                            this.props.books.map(book =>(
+                                book.id === searchedBook.id ?
+                                shelf = book.shelf :
+                                ''
+                            ));
+
                             return (
                                 <li key={searchedBook.id}>
                                     <Book
                                      book={searchedBook}
                                      moveShelf={this.props.moveShelf}
+                                     currentShelf={shelf}
                                     />
                                 </li>    
                             );
